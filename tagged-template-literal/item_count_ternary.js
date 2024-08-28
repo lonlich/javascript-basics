aa4e8c771da04221c07ae3f21e4aee403dd5a64f
lonlich <30961049+lonlich@users.noreply.github.com>
2024-08-28 18:45:33 +0300

    Do item_count_ternary exercise

--- tagged-template-literal/item_count_ternary.js
+++ tagged-template-literal/item_count_ternary.js
@@ -1,1 +1,17 @@
-copy from simple version
+//Exercise: Create a tagged template literal function called itemCount that takes a count of items and returns a message with proper pluralization. If the count is 1, the message should use "item"; otherwise, it should use "items". For example, "You have 3 items."
+
+//use ternary operator
+
+const numberOfItems = +prompt('How many items?');
+
+function itemCount(strings, numberOfItemsExp) {
+    return numberOfItemsExp <= 0
+    ? `${strings[0]}no items`
+    : numberOfItems === 1 
+        ? `${strings[0]}${numberOfItemsExp} item`
+        : `${strings[0]}${numberOfItemsExp} items`
+}
+
+const message = itemCount`You have ${numberOfItems}`;
+
+alert(message);
