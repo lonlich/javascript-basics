d1ef43db9c2e6de9b1dfa26e4b3d67d4ee629674
lonely <orenkin@yandex.ru>
2024-10-22 14:38:02 +0300

    Add multiple number input and repeated = operations

--- sketchpad/calculator/calculator.js
+++ sketchpad/calculator/calculator.js
@@ -5,70 +5,100 @@
 //функция operate, которая принимает 2 числа и тип оператора
 
 function add(num1, num2) {
-    console.log(`${num1} + ${num2} = ${result}`);
     return num1 + num2;
 }
 
 function subtract(num1, num2) {
-    console.log(`${num1} - ${num2} = ${result}`);
     return num1 - num2;
 }
 
 function multiply(num1, num2) {
-    console.log(`${num1} * ${num2} = ${result}`);
     return num1 * num2;
 }
 
 function divide(num1, num2) {
-    console.log(`${num1} / ${num2} = ${result}`);
     return num1 / num2;
 }
 
-function render() {
-
-}
+// function getNumValue(e) {
+//     const value += e.target.value;
+// }
 
 let num1 = null;
 let num2 = null;
 let result = null;
+let operation = null;
+let firstNumAssigned = false;
 
-function operate(num1 = result, num2, operation) {
+
+function operate(input1, input2, operation) {
+    console.log(`num1 before switch: ${num1}`);
+    
+    let operationFunction;
 
     switch (operation) {
-        case '+':
-        result = add(num1, num2);
-        
-        case '-':
-        result = subtract(num1, num2);
-        console.log(`${num1} + ${num2} = ${result}`);
-        case 'x':
-        result = multiply(num1, num2);
-        console.log(`${num1} + ${num2} = ${result}`);
-        case '/':
-        result = add(num1, num2);
-        console.log(`${num1} + ${num2} = ${result}`);
+        case '+': 
+        operationFunction = add;
+        break;
+    
+        case '-': 
+        operationFunction = subtract;
+        break;
+    
+        case 'x': 
+        operationFunction = multiply;
+        break;
+    
+        case '/': 
+        operationFunction = divide;
+        break;
     }
-
-
-    if (operation === 'add') {
-        result = add(num1, num2);
-        console.log(`${num1} + ${num2} = ${result}`);
-        
-    }
+    result = operationFunction(input1, input2);
+    console.log(`${input1} ${operation} ${input2} = ${result}`);
+    
+    num1 = null;
+    firstNumAssigned = false;
 }
 
 const numbers = document.querySelector('#numbers');
+let num1Input = '';
 
 numbers.addEventListener('click', (e) => {
-    
-    if (!num1) {
-        num1 = +e.target.textContent;
-    }
+    if (!firstNumAssigned) {
+        
+        num1Input += e.target.textContent;
+        num1 = +num1Input;
 
-    num2 = +e.target.textContent;
-    console.log(num2);
+        //firstNumAssigned = true;
+        console.log(num1);
+    } else {
+        num2 = +e.target.textContent;
+        console.log(num2);
+    }
 });
 
-operate(1, 2, 'add');
-operate(undefined, 2, 'add');
-operate(undefined, 10, 'add');
+
+const operations = document.querySelector('#operations');
+operations.addEventListener('click', (e) => {
+    firstNumAssigned = true;    
+    operation = e.target.textContent;
+
+});
+
+const equals = document.querySelector('#equals');
+
+equals.addEventListener('click', () => {
+    console.log(`num1 inside ev ${num1}`);
+    if (!firstNumAssigned) {
+        num1 = result;
+    }
+    operate(num1, num2, operation);
+    num1 = result;
+    num1Input = '';
+});
+
+// operate(1, 2, '+');
+// operate(undefined, 5, '+');
+// operate(undefined, 10, 'add');
+
+//undefined не работает (значение всегда присваивается)
